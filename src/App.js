e6536d41a773f7976e692a2431a961f0159ac7d2
Ju640 <67836937+Ju640@users.noreply.github.com>
2021-05-14 17:46:44 -0500

    created basic layout

--- src/App.js
+++ src/App.js
@@ -1,7 +1,35 @@
 import "./App.css";
 
+import React from "react";
+
 function App() {
-  return <div className="App">Recipes by Judy</div>;
+  return (
+    <div className="App">
+      <div className="container">
+        <div className="websiteName">Recipes by Judy</div>
+        <div className="row align-items-center">
+          <div className="col">
+            <a href="favorites.js">favorites</a>
+          </div>
+          <div className="col">Beef</div>
+          <div className="col">Chicken</div>
+        </div>
+        <div className="row align-items-center">
+          <div className="col">Vegetarian</div>
+          <div className="col">Fish</div>
+          <div className="col">Turkey</div>
+        </div>
+        <div className="row align-items-center">
+          <div className="col">Pork</div>
+          <div className="col">Slow Cooker</div>
+          <div className="col">Sides</div>
+        </div>
+        <div className="row align-items-center">
+          <div className="col">Desserts</div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
